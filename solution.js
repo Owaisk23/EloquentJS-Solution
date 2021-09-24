41c116b57f5bd6c49545474f2aeb1dd6e16702c7
Owais Ahmed Khan <owais1566@gmail.com>
2021-09-24 17:46:28 +0500

    chap 4 solution

--- solution.js
+++ solution.js
@@ -86,4 +86,123 @@
 //   console.log(countChar("kallarkahar", "a"));
 //   // → 4
 
+//Chap 4 Data Structures: Object & Array
+//The sum of range
 
+// function range(start, end, step = start < end ? 1 : -1) {
+//     let array = [];
+  
+//     if (step > 0) {
+//       for (let i = start; i <= end; i += step) array.push(i);
+//     } else {
+//       for (let i = start; i >= end; i += step) array.push(i);
+//     }
+//     return array;
+//   }
+  
+//   function sum(array) {
+//     let total = 0;
+//     for (let value of array) {
+//       total += value;
+//     }
+//     return total;
+//   }
+  
+// console.log(range(1, 10))
+// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(range(5, 2, -1));
+// // → [5, 4, 3, 2]
+// console.log(sum(range(1, 10)));
+
+//---------------------------------------------------------------------
+//Reversing an array
+
+// function reverseArray(array) {
+//     let output = [];
+//     for (let i = array.length - 1; i >= 0; i--) {
+//       output.push(array[i]);
+//     }
+//     return output;
+//   }
+  
+//   function reverseArrayInPlace(array) {
+//     for (let i = 0; i < Math.floor(array.length / 2); i++) {
+//       let old = array[i];
+//       array[i] = array[array.length - 1 - i];
+//       array[array.length - 1 - i] = old;
+//     }
+//     return array;
+//   }
+  
+//   console.log(reverseArray(["A", "B", "C"]));
+//   // → ["C", "B", "A"];
+//   let arrayValue = [1, 2, 3, 4, 5];
+//   reverseArrayInPlace(arrayValue);
+//   console.log(arrayValue);
+//   // → [5, 4, 3, 2, 1]
+
+//---------------------------------------------------------------------
+//A list
+
+// function arrayToList(array) {
+//     let list = null;
+//     for (let i = array.length - 1; i >= 0; i--) {
+//       list = {value: array[i], rest: list};
+//     }
+//     return list;
+//   }
+  
+//   function listToArray(list) {
+//     let array = [];
+//     for (let node = list; node; node = node.rest) {
+//       array.push(node.value);
+//     }
+//     return array;
+//   }
+  
+//   function prepend(value, list) {
+//     return {value, rest: list};
+//   }
+  
+//   function nth(list, n) {
+//     if (!list) return undefined;
+//     else if (n == 0) return list.value;
+//     else return nth(list.rest, n - 1);
+//   }
+  
+//   console.log(arrayToList([10, 20]));
+//   // → {value: 10, rest: {value: 20, rest: null}}
+//   console.log(listToArray(arrayToList([10, 20, 30])));
+//   // → [10, 20, 30]
+//   console.log(prepend(10, prepend(20, null)));
+//   // → {value: 10, rest: {value: 20, rest: null}}
+//   console.log(nth(arrayToList([10, 20, 30]), 1));
+//   // → 20
+
+//---------------------------------------------------------------------
+//Deep Comparison
+
+// function deepEqual(a, b) {
+//     if (a === b) return true;
+    
+//     if (a == null || typeof a != "object" ||
+//         b == null || typeof b != "object") return false;
+  
+//     let keysA = Object.keys(a), keysB = Object.keys(b);
+  
+//     if (keysA.length != keysB.length) return false;
+  
+//     for (let key of keysA) {
+//       if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+//     }
+  
+//     return true;
+//   }
+  
+//   let obj = {here: {is: "an"}, object: 2};
+//   console.log(deepEqual(obj, obj));
+//   // → true
+//   console.log(deepEqual(obj, {here: 1, object: 2}));
+//   // → false
+//   console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+//   // → true
