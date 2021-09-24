6b8bb7a37c9e69826af6da7aa1222bbf4e3a92db
Owais Ahmed Khan <owais1566@gmail.com>
2021-09-25 03:14:08 +0500

    chap 5 solution

--- solution.js
+++ solution.js
@@ -206,3 +206,92 @@
 //   // → false
 //   console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 //   // → true
+
+//Chap 2 Program Structure
+//Flattening
+
+// let arrays = [[1, 2, 3], [4, 5], [6]];
+
+// console.log(arrays.reduce((flat, current) => flat.concat(current), []));
+// // → [1, 2, 3, 4, 5, 6]
+
+//---------------------------------------------------------------------
+//Your own loop
+
+// function loop(start, test, update, body) {
+//     for (let value = start; test(value); value = update(value)) {
+//       body(value);
+//     }
+//   }
+  
+//   loop(3, n => n > 0, n => n - 1, console.log);
+//   // → 3
+//   // → 2
+//   // → 1
+
+//---------------------------------------------------------------------
+//Everything
+
+// function every(array, predicate) {
+//     for (let element of array) {
+//       if (!predicate(element)) return false;
+//     }
+//     return true;
+//   }
+  
+//   function every2(array, predicate) {
+//     return !array.some(element => !predicate(element));
+//   }
+  
+//   console.log(every([1, 3, 5], n => n < 10));
+//   // → true
+//   console.log(every([2, 4, 16], n => n < 10));
+//   // → false
+//   console.log(every([], n => n < 10));
+//   // → true
+
+
+//---------------------------------------------------------------------
+//Dominant writing direction
+
+// function characterScript(code) {
+//     for (let script of SCRIPTS) {
+//       if (script.ranges.some(([from, to]) => {
+//         return code >= from && code < to;
+//       })) {
+//         return script;
+//       }
+//     }
+//     return null;
+//   }
+
+// function countBy(items, groupName) {
+//     let counts = [];
+//     for (let item of items) {
+//       let name = groupName(item);
+//       let known = counts.findIndex(c => c.name == name);
+//       if (known == -1) {
+//         counts.push({name, count: 1});
+//       } else {
+//         counts[known].count++;
+//       }
+//     }
+//     return counts;
+//   }
+  
+
+// function dominantDirection(text) {
+//     let counted = countBy(text, char => {
+//       let script = characterScript(char.codePointAt(0));
+//       return script ? script.direction : "none";
+//     }).filter(({name}) => name != "none");
+  
+//     if (counted.length == 0) return "ltr";
+  
+//     return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+//   }
+  
+//   console.log(dominantDirection("Hello!"));
+//   // → ltr
+//   console.log(dominantDirection("Hey, مساء الخير"));
+//   // → rtl
