286a7c4eb1eb3e28c0badc9e9688607ce7f304fe
Owais Ahmed Khan <owais1566@gmail.com>
2021-09-27 23:36:51 +0500

    added chap 6 solution

--- solution.js
+++ solution.js
@@ -41,7 +41,7 @@
 //     if (a < b) return a;
 //     else return b;
 // }
-  
+
 // console.log(min(0, 10));
 // // → 0
 // console.log(min(0, -10));
@@ -56,7 +56,7 @@
 //     else if (n < 0) return isEven(-n);
 //     else return isEven(n - 2);
 //   }
-  
+
 //   console.log(isEven(78));
 //   // → true
 //   console.log(isEven(23));
@@ -76,11 +76,11 @@
 //     }
 //     return counted;
 //   }
-  
+
 //   function countBs(string) {
 //     return countChar(string, "B");
 //   }
-  
+
 //   console.log(countBs("BBC"));
 //   // → 2
 //   console.log(countChar("kallarkahar", "a"));
@@ -91,7 +91,7 @@
 
 // function range(start, end, step = start < end ? 1 : -1) {
 //     let array = [];
-  
+
 //     if (step > 0) {
 //       for (let i = start; i <= end; i += step) array.push(i);
 //     } else {
@@ -99,7 +99,7 @@
 //     }
 //     return array;
 //   }
-  
+
 //   function sum(array) {
 //     let total = 0;
 //     for (let value of array) {
@@ -107,7 +107,7 @@
 //     }
 //     return total;
 //   }
-  
+
 // console.log(range(1, 10))
 // // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(range(5, 2, -1));
@@ -124,7 +124,7 @@
 //     }
 //     return output;
 //   }
-  
+
 //   function reverseArrayInPlace(array) {
 //     for (let i = 0; i < Math.floor(array.length / 2); i++) {
 //       let old = array[i];
@@ -133,7 +133,7 @@
 //     }
 //     return array;
 //   }
-  
+
 //   console.log(reverseArray(["A", "B", "C"]));
 //   // → ["C", "B", "A"];
 //   let arrayValue = [1, 2, 3, 4, 5];
@@ -151,7 +151,7 @@
 //     }
 //     return list;
 //   }
-  
+
 //   function listToArray(list) {
 //     let array = [];
 //     for (let node = list; node; node = node.rest) {
@@ -159,17 +159,17 @@
 //     }
 //     return array;
 //   }
-  
+
 //   function prepend(value, list) {
 //     return {value, rest: list};
 //   }
-  
+
 //   function nth(list, n) {
 //     if (!list) return undefined;
 //     else if (n == 0) return list.value;
 //     else return nth(list.rest, n - 1);
 //   }
-  
+
 //   console.log(arrayToList([10, 20]));
 //   // → {value: 10, rest: {value: 20, rest: null}}
 //   console.log(listToArray(arrayToList([10, 20, 30])));
@@ -184,21 +184,21 @@
 
 // function deepEqual(a, b) {
 //     if (a === b) return true;
-    
+
 //     if (a == null || typeof a != "object" ||
 //         b == null || typeof b != "object") return false;
-  
+
 //     let keysA = Object.keys(a), keysB = Object.keys(b);
-  
+
 //     if (keysA.length != keysB.length) return false;
-  
+
 //     for (let key of keysA) {
 //       if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
 //     }
-  
+
 //     return true;
 //   }
-  
+
 //   let obj = {here: {is: "an"}, object: 2};
 //   console.log(deepEqual(obj, obj));
 //   // → true
@@ -207,7 +207,7 @@
 //   console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 //   // → true
 
-//Chap 2 Program Structure
+//Chap 5 Higher Order Function
 //Flattening
 
 // let arrays = [[1, 2, 3], [4, 5], [6]];
@@ -223,7 +223,7 @@
 //       body(value);
 //     }
 //   }
-  
+
 //   loop(3, n => n > 0, n => n - 1, console.log);
 //   // → 3
 //   // → 2
@@ -238,11 +238,11 @@
 //     }
 //     return true;
 //   }
-  
+
 //   function every2(array, predicate) {
 //     return !array.some(element => !predicate(element));
 //   }
-  
+
 //   console.log(every([1, 3, 5], n => n < 10));
 //   // → true
 //   console.log(every([2, 4, 16], n => n < 10));
@@ -278,20 +278,160 @@
 //     }
 //     return counts;
 //   }
-  
+
 
 // function dominantDirection(text) {
 //     let counted = countBy(text, char => {
 //       let script = characterScript(char.codePointAt(0));
 //       return script ? script.direction : "none";
 //     }).filter(({name}) => name != "none");
-  
+
 //     if (counted.length == 0) return "ltr";
-  
+
 //     return counted.reduce((a, b) => a.count > b.count ? a : b).name;
 //   }
-  
+
 //   console.log(dominantDirection("Hello!"));
 //   // → ltr
 //   console.log(dominantDirection("Hey, مساء الخير"));
 //   // → rtl
+
+//Chap 6 The secret life of object
+//A vector type
+// class Vec {
+//     constructor(x, y) {
+//       this.x = x;
+//       this.y = y;
+//     }
+
+//     plus(other) {
+//       return new Vec(this.x + other.x, this.y + other.y);
+//     }
+
+//     minus(other) {
+//       return new Vec(this.x - other.x, this.y - other.y);
+//     }
+
+//     get length() {
+//       return Math.sqrt(this.x * this.x + this.y * this.y);
+//     }
+//   }
+
+//   console.log(new Vec(11, 23).plus(new Vec(23, 6)));
+//   // → Vec{x: 34, y: 29}
+//   console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+//   // → Vec{x: -1, y: -1}
+//   console.log(new Vec(3, 4).length);
+//   // → 5
+
+//---------------------------------------------------------------------
+//Groups
+// class Group {
+//     constructor() {
+//       this.members = [];
+//     }
+
+//     add(value) {
+//       if (!this.has(value)) {
+//         this.members.push(value);
+//       }
+//     }
+
+//     delete(value) {
+//       this.members = this.members.filter(v => v !== value);
+//     }
+
+//     has(value) {
+//       return this.members.includes(value);
+//     }
+
+//     static from(collection) {
+//       let group = new Group;
+//       for (let value of collection) {
+//         group.add(value);
+//       }
+//       return group;
+//     }
+//   }
+
+//   let group = Group.from([10, 20]);
+//   console.log(group.has(10));
+//   // → true
+//   console.log(group.has(30));
+//   // → false
+//   group.add(10);
+//   group.delete(10);
+//   console.log(group.has(10));
+//   // → false
+
+//---------------------------------------------------------------------
+//Iterable groups
+
+// class Group {
+//     constructor() {
+//         this.members = [];
+//     }
+
+//     add(value) {
+//         if (!this.has(value)) {
+//             this.members.push(value);
+//         }
+//     }
+
+//     delete(value) {
+//         this.members = this.members.filter(v => v !== value);
+//     }
+
+//     has(value) {
+//         return this.members.includes(value);
+//     }
+
+//     static from(collection) {
+//         let group = new Group;
+//         for (let value of collection) {
+//             group.add(value);
+//         }
+//         return group;
+//     }
+
+//     [Symbol.iterator]() {
+//         return new GroupIterator(this);
+//     }
+// }
+
+// class GroupIterator {
+//     constructor(group) {
+//         this.group = group;
+//         this.position = 0;
+//     }
+
+//     next() {
+//         if (this.position >= this.group.members.length) {
+//             return { done: true };
+//         } else {
+//             let result = {
+//                 value: this.group.members[this.position],
+//                 done: false
+//             };
+//             this.position++;
+//             return result;
+//         }
+//     }
+// }
+
+// for (let value of Group.from(["a", "b", "c"])) {
+//     console.log(value);
+// }
+// // → a
+// // → b
+// // → c
+
+//---------------------------------------------------------------------
+//Borrowing a method
+
+// let map = {one: true, two: true, hasOwnProperty: true};
+
+// console.log(Object.prototype.hasOwnProperty.call(map, "one"));
+// // → true
+
+
