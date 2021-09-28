698cb795bc2a537f871646adb4d87989fea776fa
Owais Ahmed Khan <owais1566@gmail.com>
2021-09-29 00:04:40 +0500

    chap 8 solution

--- solution.js
+++ solution.js
@@ -441,31 +441,101 @@
 //     constructor(members) {
 //       this.members = members;
 //     }
-  
+
 //     add(value) {
 //       if (this.has(value)) return this;
 //       return new PGroup(this.members.concat([value]));
 //     }
-  
+
 //     delete(value) {
 //       if (!this.has(value)) return this;
 //       return new PGroup(this.members.filter(m => m !== value));
 //     }
-  
+
 //     has(value) {
 //       return this.members.includes(value);
 //     }
 //   }
-  
+
 //   PGroup.empty = new PGroup([]);
-  
+
 //   let a = PGroup.empty.add("a");
 //   let ab = a.add("b");
 //   let b = ab.delete("a");
-  
+
 //   console.log(b.has("b"));
 //   // → true
 //   console.log(a.has("b"));
 //   // → false
 //   console.log(b.has("a"));
 //   // → false
+
+//Chap 8 Bugs and errors
+//Retry
+
+// class MultiplicatorUnitFailure extends Error {}
+
+// function primitiveMultiply(a, b) {
+//   if (Math.random() < 0.2) {
+//     return a * b;
+//   } else {
+//     throw new MultiplicatorUnitFailure("Klunk");
+//   }
+// }
+
+// function reliableMultiply(a, b) {
+//   for (;;) {
+//     try {
+//       return primitiveMultiply(a, b);
+//     } catch (e) {
+//       if (!(e instanceof MultiplicatorUnitFailure))
+//         throw e;
+//     }
+//   }
+// }
+
+// console.log(reliableMultiply(8, 8));
+// // → 64
+
+//---------------------------------------------------------------------
+//The locked box
+
+// const box = {
+//     locked: true,
+//     unlock() { this.locked = false; },
+//     lock() { this.locked = true; },
+//     _content: [],
+//     get content() {
+//         if (this.locked) throw new Error("Locked!");
+//         return this._content;
+//     }
+// };
+
+// function withBoxUnlocked(body) {
+//     let locked = box.locked;
+//     if (!locked) {
+//         return body();
+//     }
+
+//     box.unlock();
+//     try {
+//         return body();
+//     } finally {
+//         box.lock();
+//     }
+// }
+
+// withBoxUnlocked(function () {
+//     box.content.push("gold piece");
+// });
+// try {
+//     withBoxUnlocked(function () {
+//         throw new Error("Pirates on the horizon! Abort!");
+//     });
+// } catch (e) {
+//     console.log("Error raised:", e);
+// }
+
+// console.log(box.locked);
+//   // → true
+
